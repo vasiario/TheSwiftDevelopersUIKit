b0845440f40f107c322440e145fdbee000f575d5
vasiario <vasiariov@gmail.com>
2023-04-17 20:17:37 +0700

    added first tasks

---
+++ 1task/3AlertSecondTaskMVC/3AlertSecondTaskMVC/Model/SettingsAllert.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsAllert.swift
+//  3AlertSecondTaskMVC
+//
+//  Created by vasiario on 17.04.2023.
+//
+
+import UIKit
+
+struct Model {
+
+static let model = Model()
+    
+    func convertString(string: String) -> String {
+       return String(string.reversed())
+    }
+}
---
+++ 1task/3AlertSecondTaskMVC/3AlertSecondTaskMVC/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  3AlertSecondTaskMVC
+//
+//  Created by vasiario on 17.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func CentralButtonPressed(_ sender: Any) {
+        returnResultAllert(title: "Введите слово", message: "(можно отзеркаленное) Например: olleH", preferredStyle: .alert)
+    }
+    
+    func returnResultAllert(title: String, message: String, preferredStyle: UIAlertController.Style) {
+        let allertNumbersController = UIAlertController(title: title, message: message, preferredStyle: preferredStyle)
+        let actionAllertNumbersController = UIAlertAction(title: "Отобразить зеркально", style: .default) { action in
+            self.textLabel?.text = Model.model.convertString(string: allertNumbersController.textFields?.first?.text ?? "")
+            
+        }
+        
+        allertNumbersController.addTextField()
+        allertNumbersController.addAction(actionAllertNumbersController)
+        self.present(allertNumbersController, animated: true)
+    }
+}
+
